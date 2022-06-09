50946e603114b2fb31a8a5425d5a8857d7f60e07
seanmcauliffe <seanmcauliffe@uvic.ca>
2022-06-08 17:38:08 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Algorithm 2: Class Offering Capacity Forecaster
+
+This repository contains a small package, *Class-Capacity-Forecaster*, for
+estimating required course offering capacities based on historical enrollment
+data. 
+
+This package was authored for the Company 2 course scheduler project by the Company2-Algorithm2 subsystem team.
+
+## Setup
+
+Clone this repo into the scheduler (or backend) repository. Inside the algorithm 2 module directory,
+build and install the package as follows:
+
+```bash
+$ python3 -m build
+$ pip3 install ./dist/Class-Capacity-Forecaster-0.0.1.tar
+```
+
+The algorithm 2 module may then be imported into and called from the backend.
+
+```python
+import Class-Capacity-Forecaster
+```
+
---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Class-Capacity-Forecaster",
+    version="0.0.1",
+    author="Algorithm 2 SubTeam",
+    author_email="N/A",
+    description="An algorithm for predicting future class capacities based on historical enrollment",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/seng499-company2/algorithm2",
+    project_urls={
+        "Bug Tracker": "https://github.com/seng499-company2/algorithm2/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
