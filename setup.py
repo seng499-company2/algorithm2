30a7a7f800e8f192d94f1df0948090990dc9b169
SeanMcAuliffe <sean.tristan.mcauliffe@gmail.com>
2022-06-17 20:24:34 -0700

    Update version number control mechanism

--- setup.py
+++ setup.py
@@ -1,14 +1,35 @@
 import setuptools
+from urllib.request import urlopen
+from lxml import etree
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-with open("version.txt", "r") as fv:
-    version = fv.read()
+# Inspect current most recent version on PyPi index
+url =  "https://test.pypi.org/project/Class-Capacity-Forecaster/"
+response = urlopen(url)
+htmlparser = etree.HTMLParser()
+tree = etree.parse(response, htmlparser)
+element = tree.xpath("/html/body/main/div[2]/div/div[1]/h1")
+text = element[0].text.strip()
+pypi_version = text.split(' ')[1]
+
+major = int(pypi_version[0])
+minor = int(pypi_version[2])
+patch = int(pypi_version[4])
+patch += 1
+if patch > 9:
+    patch = 0
+    minor += 1
+if minor > 9:
+    minor = 0
+    major += 1
+
+release_version = f"{major}.{minor}.{patch}"
 
 setuptools.setup(
     name="Class-Capacity-Forecaster",
-    version=version,
+    version=release_version,
     author="Algorithm 2 SubTeam",
     author_email="seanmcauliffe42@gmail.com",
     description="An algorithm for predicting future class capacities based on historical enrollment",
