dad282b09dd8e07550e0e737a0d1622dd06b4935
SeanMcAuliffe <sean.tristan.mcauliffe@gmail.com>
2022-06-17 19:27:49 -0700

    Add manifest file to contain package data

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-with open("./version.txt", "r") as fv:
+with open("version.txt", "r") as fv:
     version = fv.read()
 
 setuptools.setup(
@@ -25,5 +25,6 @@ setuptools.setup(
     ],
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
+    include_package_data=True,
     python_requires=">=3.6",
 )
