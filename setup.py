60feb9d0911ab2cce85658a3c68540084ec28fbf
Tristan Cusi <cusitristan@gmail.com>
2022-06-17 17:26:32 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-with open("./version.txt", "r") as fv:
+with open("version.txt", "r") as fv:
     version = fv.read()
 
 setuptools.setup(
