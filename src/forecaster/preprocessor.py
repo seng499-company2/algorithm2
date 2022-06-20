47de0964f63bb32821ae945b137274e00b9d7fae
seanmcauliffe <seanmcauliffe@uvic.ca>
2022-06-20 15:02:17 -0700

    Implement compute_bounds

--- src/forecaster/preprocessor.py
+++ src/forecaster/preprocessor.py
@@ -3,21 +3,34 @@
 # Date: June 17th, 2022
 # This module preprocesses data for use in the forecaster
 
+import math
+
+
 # Private Module Variables, Classes
+_MIN_COURSES = 1
+_MAX_COURSES = 6
+_RATIO_ACADEMIC = float(11/15)
+
 # Private Module Helper Functions
 # API Functions
+
 
 def compute_bounds(program_enrolment: dict) -> (int, int):
     """ This function computes the upper and lower bound on the global
     seat allocation
 
-    :param program_enrolment:
-    :return:
+    :param program_enrolment: historical course enrollment data
+    :return: lower and upper bound for global seat assignment
     """
 
-    lower_bound = 0
-    upper_bound = 100
-    # Implement this
+    prev_enrollment = 0
+    for year in program_enrolment["2021"].keys():
+        prev_enrollment += program_enrolment["2021"][year]
+
+    current_enrolment = math.ceil(prev_enrollment * 1.0855)
+    lower_bound = _MIN_COURSES * math.ceil((current_enrolment * _RATIO_ACADEMIC))
+    upper_bound = _MAX_COURSES * math.ceil((current_enrolment * _RATIO_ACADEMIC))
+
     return lower_bound, upper_bound
 
 
