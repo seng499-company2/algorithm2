e48b6e83aa06a7c88a6af099a32cb140db389ae0
Tristan Cusi <cusitristan@gmail.com>
2022-06-24 00:42:39 -0700

    preprocess function
    -added preprocess function to preprocessor
    -added tests
    -added an enrolment data generator

--- src/forecaster/preprocessor.py
+++ src/forecaster/preprocessor.py
@@ -3,9 +3,12 @@
 # Date: June 17th, 2022
 # This module preprocesses data for use in the forecaster
 
+import json
+
 # Private Module Variables, Classes
 # Private Module Helper Functions
 # API Functions
+
 
 def compute_bounds(program_enrolment: dict) -> (int, int):
     """ This function computes the upper and lower bound on the global
@@ -21,12 +24,106 @@ def compute_bounds(program_enrolment: dict) -> (int, int):
     return lower_bound, upper_bound
 
 
-def pre_process(course_enrollment: dict) -> dict:
-    """ Takes class enrollment JSON and generates an intermediate object with data-series
-     organized by class-term
+def pre_process(course_enrollment: list, schedule: dict) -> dict:
+    """ Takes class enrollment JSON and schedule JSON and generates an 
+    intermediate object with data-series organized by class-term
 
     :param course_enrollment: object loaded from course enrolment JSON file
+    :param schedule: JSON schedule object
+
     :return: internal intermediate course data series object
     """
+    
+    # create a list of course-term codes for each term 
+    historical_term_codes = get_historical_term_codes(course_enrollment)
 
-    return {"CSC110-F": {"data": [0, 0, 500, 200, 400], "approach": 0, "capacity": 500}}
+    # create a set of all course codes
+    set_of_courses = get_set_of_course_codes(schedule)
+
+    intermediate = {}
+    for course in set_of_courses:
+        for term in ('fall', 'spring', 'summer'):
+            if not is_course_in_term(course, schedule[term]):
+                continue
+            term_abbreviation = {'fall': 'F', 'spring': 'SP', 'summer': 'SU'}
+            key = course + '-' + term_abbreviation[term]
+            
+            #check if course already has capacity from schedule
+            capacity = get_capacity(course, schedule[term])
+            if capacity > 0:
+                intermediate[key] = {'data': None, 'approach': 0, \
+                    'capacity': capacity}
+
+            data = []
+            # for each offering in (200801, 200805....)
+            for offering in historical_term_codes[term]:
+                # loop through course_enrollment to get enrollment data
+                students_enrolled = 0
+                for section in course_enrollment:
+                    if ((section["term"] == offering) and\
+                            (section["subjectCourse"] == course)):
+                        # add up sections enrollments
+                        students_enrolled = students_enrolled \
+                            + section['enrollment']
+                data.append(students_enrolled)
+            intermediate[key] = {'data': data, 'approach': 0, 'capacity': 0}
+
+    return intermediate 
+
+
+def get_historical_term_codes(course_enrollment: list) -> dict:
+    """ This function takes in the historical enrollments, counts up the 
+    term-course offering codes (eg 201801) and separates them into three terms
+    fall, spring and summer"""
+    historical_term_codes = {'fall':[], 'spring': [], 'summer': []}
+
+    for course_term_offering in course_enrollment:
+        if course_term_offering['term'].endswith('09'):
+            historical_term_codes['fall']\
+            .append(course_term_offering['term'])
+        elif course_term_offering['term'].endswith('01'):
+            historical_term_codes['spring']\
+            .append(course_term_offering['term'])
+        elif course_term_offering['term'].endswith('05'):
+            historical_term_codes['summer']\
+            .append(course_term_offering['term'])
+
+    for term in ('fall', 'spring', 'summer'):
+        historical_term_codes[term] = remove_dups(historical_term_codes[term])
+        historical_term_codes[term].sort()
+
+    return historical_term_codes
+
+def remove_dups(input_list: list) -> list:
+    """ This function removes duplicates from a list and returns that list"""
+    return list(dict.fromkeys(input_list))
+
+def get_set_of_course_codes(schedule: dict) -> set:
+    """ This function takes in a schedule object and returns a set (unique
+    list) of all course codes in that schedule"""
+    list_of_courses = []
+
+    for term in schedule.values():
+        for course_offering in term:
+            list_of_courses.append(course_offering['course']['code'])
+    return set(list_of_courses)
+
+
+def is_course_in_term(course_code: str, course_obj_list: list) -> bool:
+    """ This function checks if a course is in the given schedule term course
+    list"""
+    for course_obj in course_obj_list:
+        if course_obj['course']['code'] == course_code:
+            return True
+    return False
+
+
+def get_capacity(course_code: str, course_offerings: list) -> int:
+    """ This function retrieves the cumulative capacity for all sections of a
+    course in a specified term"""
+    capacity = 0;
+    for course_offering in course_offerings:
+        if course_offering['course']['code'] == course_code:
+            for section in course_offering['sections']:
+                capacity = capacity + int(section['capacity'])
+    return capacity
--- test/test_preprocessor.py
+++ test/test_preprocessor.py
@@ -1,6 +1,28 @@
 import pytest
 import json
 from forecaster import preprocessor
+from mock_data_generator import mock_data_generator
+
+
+def generate_historial_offering(course: str, term: str, enrollment: int) -> dict:
+    return {'term': term, 'enrollment': enrollment, 'subjectCourse': course}
+
+
+def generate_course_enrollment(fall_courses: list, spring_courses: list, summer_courses: list, historical_terms: list) -> dict:
+    course_enrollment = []
+    for course_list in (fall_courses, spring_courses, summer_courses):
+        for course in course_list:
+            for historical_term in historical_terms:
+                if((course_list == fall_courses) and (historical_term.endswith('09'))):
+                    course_enrollment\
+                    .append(generate_historial_offering(course, historical_term, 1))
+                elif((course_list == spring_courses) and (historical_term.endswith('01'))):
+                    course_enrollment\
+                    .append(generate_historial_offering(course, historical_term, 1))
+                elif((course_list == summer_courses) and (historical_term.endswith('05'))):
+                    course_enrollment\
+                    .append(generate_historial_offering(course, historical_term, 1))
+    return course_enrollment
 
 
 def test_computeBounds():
@@ -9,8 +31,63 @@ def test_computeBounds():
     assert high_bound == 100
 
 
+def test_get_historical_term_codes():
+    course_enrollment_test = generate_course_enrollment(['CSC225','SENG275'],['CSC226'],['CSC230'],['202109', '202201', '202205', '202209', '202301'])
+
+    historical_term_codes = preprocessor.get_historical_term_codes(course_enrollment_test)
+    assert historical_term_codes == {'fall': ['202109', '202209'], 
+                                    'spring': ['202201','202301'], 
+                                    'summer': ['202205']}
+
+
+def test_remove_dups():
+    list1 = [1,1,2,2,3,4]
+    list2 = preprocessor.remove_dups(list1)
+    list3 = ['a','a','b','b','c','d']
+    list4 = preprocessor.remove_dups(list3)
+
+    assert list2 == [1,2,3,4]
+    assert list4 == ['a','b','c','d']
+
+
+def test_get_set_of_course_codes():
+    schedule_test = mock_data_generator(['CSC225','SENG275'],['CSC226'],['CSC230'], 1, 1)
+    set_of_courses = preprocessor.get_set_of_course_codes(schedule_test)
+
+    assert set_of_courses == {'CSC225','SENG275','CSC226','CSC230'}
+
+
+def test_is_course_in_term():
+    schedule_test = mock_data_generator(['CSC225','SENG275'],['CSC226'],['CSC230'], 1, 1)
+
+    assert preprocessor.is_course_in_term('CSC225', schedule_test['fall'])
+    assert preprocessor.is_course_in_term('SENG275', schedule_test['fall'])
+    assert not preprocessor.is_course_in_term('CSC226', schedule_test['fall'])
+    assert preprocessor.is_course_in_term('CSC226', schedule_test['spring'])
+    assert preprocessor.is_course_in_term('CSC230', schedule_test['summer'])
+
+def test_get_capacity():
+    schedule_test = mock_data_generator(['CSC225','SENG275'],['CSC226'],['CSC230'], 1, 1)
+    section_template = {'professor': '', 'capacity': '10'}
+    schedule_test['fall'][0]['sections'].append(section_template)
+    schedule_test['fall'][0]['sections'].append(section_template)
+
+    capacity = preprocessor.get_capacity('CSC225', schedule_test['fall'])
+
+    assert capacity == 20
+
+
 def test_preprocessor():
-    data = preprocessor.pre_process({})
-    assert data["CSC110-F"]["data"] == [0, 0, 500, 200, 400]
-    assert data["CSC110-F"]["approach"] == 0
-    assert data["CSC110-F"]["capacity"] == 500
+    schedule_test = mock_data_generator(['CSC225','SENG275'],['CSC226'],['CSC230'], 1, 1)
+    course_enrollment_test = generate_course_enrollment(['CSC225','SENG275'] ,
+                                                        ['CSC226'], 
+                                                        ['CSC230'], 
+                                                        ['202109', '202201', '202205', '202209', '202301'])
+    data = preprocessor.pre_process(course_enrollment_test, schedule_test)
+
+    assert data == {'CSC225-F': {'data': [1, 1], 'approach': 0, 'capacity': 0},
+                    'SENG275-F': {'data': [1, 1], 'approach': 0, 'capacity': 0},
+                    'CSC226-SP': {'data': [1, 1], 'approach': 0, 'capacity': 0},
+                    'CSC230-SU': {'data': [1], 'approach': 0, 'capacity': 0},
+                    }
+
