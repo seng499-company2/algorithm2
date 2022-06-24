7363261c1a99bf78b37bfb4c6b3a45457f3b8cda
Jonah Koebernick <jojo52jojo52@gmail.com>
2022-06-24 11:40:38 -0700

    First pass at determination

---
+++ src/forecaster/constants.py
@@ -0,0 +1,3 @@
+# IF THESE CONFIGS ARE CHANGED PLEASE REVIEW TESTS
+MAX_TERMS_SINCE_LAST_OFFERING = 3  # The max number of years since the last offering to perform statistical forecasting
+MIN_DATA_POINTS               = 5  # Then minimum number of data points needed to perform statistical forecasting
--- src/forecaster/determination.py
+++ src/forecaster/determination.py
@@ -2,11 +2,35 @@
 # Author:
 # Date: June 17th, 2022
 # This module determines the approach to use
+from .constants import *
 
 # Private Module Variables, Classes
-# Private Module Helper Functions
-# API Functions
 
+
+# Private Module Helper Functions
+def is_year_recent_enough(course_data: list):
+    for count, capacity in enumerate(reversed(course_data)):
+        if count < MAX_TERMS_SINCE_LAST_OFFERING and capacity > 0:
+            return True
+        if count > MAX_TERMS_SINCE_LAST_OFFERING:
+            break
+
+    return False
+
+
+def is_enough_data(course_data: list):
+    count = 0
+    for capacity in course_data:
+        if capacity > 0:
+            count = count + 1
+
+    if count >= MIN_DATA_POINTS:
+        return True
+
+    return False
+
+
+# API Functions
 def determine_approach(internal_series: dict) -> None:
     """ For each course offering in the internal data series, determine
     whether to apply statical or heuristic methods for capacity assignment:
@@ -16,4 +40,18 @@ def determine_approach(internal_series: dict) -> None:
     :return: None, internal_series is modified in place
     """
 
-    internal_series["CSC110-F"]["approach"] = 1
+    for key, course in internal_series.items():
+        print(course)
+        course['approach'] = 0
+
+        # The most recent data is too old
+        if not is_year_recent_enough(course['data']):
+            continue
+        # There isn't enough data
+        if not is_enough_data(course['data']):
+            continue
+
+        # If all check passed we have enough information for statistical forecasting
+        course['approach'] = 1
+
+    return
