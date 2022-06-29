8b5e5f40ec5151b8b6711ed4497730cb2c62de56
Tristan Cusi <cusitristan@gmail.com>
2022-06-29 13:52:14 -0700

    created test_integration.py with skeleton code

---
+++ test/test_integration.py
@@ -0,0 +1,44 @@
+import pytest
+import json
+from forecaster import constants
+from forecaster import determination
+from forecaster import guarantor
+from forecaster import hueristic
+from forecaster import postprocessor
+from forecaster import preprocessor
+from forecaster import statistical
+from mock_data_generator import mock_data_generator
+from mock_data_generator import generate_course_enrollment
+
+
+def test_input_to_preprocessor():
+
+
+def test_preprocessor_to_determination():
+
+
+def test_preprocessor_to_guarantor():
+
+
+def test_determination_to_hueristic():
+
+
+def test_determination_to_statistical():
+
+
+def test_hueristic_to_guarantor():
+
+
+def test_statistical_to_guarantor():
+
+
+def test_guarantor_to_postprocessor():
+
+
+def test_postprocessor_to_output():
+
+
+def test_forcaster_with_sample_data1():
+
+
+def test_forcaster_with_sample_data2():
