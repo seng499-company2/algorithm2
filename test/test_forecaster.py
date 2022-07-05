25bca0fa4bd6b89900b94cbf1e1eb8388c2c27a4
unknown <jonahkoebernick@gmail.com>
2022-07-05 11:49:41 -0700

    Fixed file locations to conform to new dir struct

--- test/test_forecaster.py
+++ test/test_forecaster.py
@@ -4,11 +4,11 @@ from forecaster.forecaster import forecast
 
 
 def test_forecast():
-    with open("../data/mockSchedule2.json", "r") as f:
+    with open("../data/mock/mockSchedule2.json", "r") as f:
         schedule = json.load(f)
-    with open("../data/historicCourseData.json", "r") as f:
+    with open("../data/real/historicCourseData.json", "r") as f:
         class_enrollment = json.load(f)
-    with open("../data/programEnrollmentData.json", "r") as f:
+    with open("../data/real/programEnrollmentData.json", "r") as f:
         program_enrollment = json.load(f)
     schedule = forecast(class_enrollment, program_enrollment, schedule)
     assert schedule is not None
