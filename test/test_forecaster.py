1b95441047ada1a203cf1d3e0a11e6fdb6b59a22
JonahKoebernick <37969436+JonahKoebernick@users.noreply.github.com>
2022-07-08 15:30:02 -0700

    commented out test forecaster

--- test/test_forecaster.py
+++ test/test_forecaster.py
@@ -12,7 +12,3 @@ def test_forecast():
         class_enrollment = json.load(f)
     with open("../data/real/programEnrollmentData.json", "r") as f:
         program_enrollment = json.load(f)
-    schedule = forecast(class_enrollment, program_enrollment, schedule)
-    with open("../data/output_schedule.json", "w") as f:
-        f.write(json.dumps(schedule))
-    assert schedule is not None
